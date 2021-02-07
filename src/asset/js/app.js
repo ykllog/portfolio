5837fe44039cc9319d75fa88e97a9bba3ee583e1
ykllog <info@ykllog.com>
2021-02-07 22:45:27 +0900

    first commit

---
+++ src/asset/js/app.js
@@ -0,0 +1,37 @@
+$(() => {
+  $(function () {
+    // ハンバーガーメニュー
+    $('#nav-toggle').on('click', function() {
+      $('body').toggleClass('open');
+    });
+    // ヘッダー固定(SP)
+    // fvを超えたらスクロールでheaderに色を付ける
+    var mainPos = $(".fv").height();
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > mainPos) {
+        $(".header__inner").addClass("addColor");
+      } else {
+        $(".header__inner").removeClass("addColor");
+      }
+    });
+    // トップに戻るリンク
+    var pagetop = $('#page_top');   
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    // ページ内リンク時スクロール
+    $('a[href^="#"]').click(function(){
+        var time = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" ? 'html' : href);
+        var distance = target.offset().top;
+        $("html, body").animate({scrollTop:distance}, time, "swing");
+        return false;
+    });
+  })
+});
